37c1b4da021360d9f4df986d4ba03b2e7d7c0e9a
Namrata <namratabca23@gmail.com>
2026-05-26 07:54:24 +0545

    this is lab 10

--- Lab_Report_10_Static_Constructors/Program.cs
+++ Lab_Report_10_Static_Constructors/Program.cs
@@ -1,1 +1,64 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Static Class
+static class Utility
+{
+    public static void ShowMessage()
+    {
+        Console.WriteLine("This is a utility method from static class.");
+    }
+}
+
+class Student
+{
+    private string name;
+    private int[] marks = new int[3];
+
+    // Static Constructor
+    static Student()
+    {
+        Console.WriteLine("Static Constructor Called");
+    }
+
+    // Property
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    // Indexer
+    public int this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+
+        // Using Property
+        s1.Name = "Namrata";
+
+        // Using Indexer
+        s1[0] = 85;
+        s1[1] = 90;
+        s1[2] = 95;
+
+        Console.WriteLine("Student Name: " + s1.Name);
+
+        Console.WriteLine("Marks:");
+        Console.WriteLine(s1[0]);
+        Console.WriteLine(s1[1]);
+        Console.WriteLine(s1[2]);
+
+        // Calling Static Class Method
+        Utility.ShowMessage();
+
+        Console.ReadLine();
+    }
+}
